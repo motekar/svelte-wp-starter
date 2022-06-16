ba34517c1e239af594d8fa6902a6d56b43fdb913
fadlee <fad.lee@hotmail.com>
2022-06-16 14:15:19 +0700

    fix path

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,5 @@
 import apiFetch from "@wordpress/api-fetch";
-import App from "./admin/App.svelte";
+import App from "./admin/AdminApp.svelte";
 import menuFix from "./admin/utils/admin-menu-fix";
 
 apiFetch.use(apiFetch.createRootURLMiddleware("/wp-json/"));
